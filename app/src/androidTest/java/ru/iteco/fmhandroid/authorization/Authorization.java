228fda04d5630b5ad6180ad2e3b4df88842c66c0
AAB-87 <andrej-abrekov@yandex.ru>
2022-10-19 22:24:35 +0400

    добавил ViewActions для тестов

--- app/src/androidTest/java/ru/iteco/fmhandroid/authorization/Authorization.java
+++ app/src/androidTest/java/ru/iteco/fmhandroid/authorization/Authorization.java
@@ -1,39 +1,31 @@
-
-import androidx.annotation.NonNull;
-import androidx.test.espresso.ViewInteraction;
+package authorization;
 
 import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.replaceText;
-import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
-import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
+import static androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.isFocusable;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
-import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withHint;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static androidx.test.espresso.action.ViewActions.click;
-
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 
-import androidx.test.espresso.matcher.ViewMatchers;
+import android.os.SystemClock;
+
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import ru.iteco.fmhandroid.R;
-import ru.iteco.fmhandroid.ui.AppActivity;
 
 @RunWith(AndroidJUnit4.class) // чтобы класс запускался как набор тестов
 
@@ -50,10 +42,12 @@ public class Authorization {
     String invalidPassword = "password";
 
     @Test
-    public void logInToTheApp() { // падает если авторизирован в приложении
+    public void logInToTheApp() throws InterruptedException { // падает если авторизирован в приложении
         ViewInteraction mainText = onView(withText("Авторизация")); // создаём ViewInteraction и указываем по id искомый элемент
         mainText.check(matches(isDisplayed())); // проверка что элемент отражается на старнице
         mainText.check(matches(withText("Авторизация"))); // и у этого эемента текс Войти
+        Thread.sleep(5500);
+//        SystemClock.sleep(3000);
     }
 
     @Test
@@ -62,20 +56,23 @@ public class Authorization {
         onView(allOf(withHint("Пароль"))).perform(replaceText(validPassword)).check(matches(withText("password2"))); // вводим пароль
         closeSoftKeyboard(); // скрываем клавиатуру ввода
         onView(withId(R.id.enter_button)).perform(click()); // кликаем по кнопк входа
-        Thread.sleep(3500);
-        onView(withId(R.id.trademark_image_view)).check(matches(isDisplayed())); // убеждаемся что вошли в приложение, отображается ВХОСПИСЕ
+        onView(withId(R.id.container_custom_app_bar_include_on_fragment_main)).check(matches(isDisplayed())); // убеждаемся что вошли в приложение, отображается ВХОСПИСЕ
+        Thread.sleep(5500);
+//        SystemClock.sleep(3000);
     }
 
     @Test
-    public void exitingTheApplication() { // работает если приложение открыто
+    public void exitingTheApplication() throws InterruptedException { // работает если приложение открыто
         onView(withId(R.id.authorization_image_button)).perform(click()); // находим иконку для выхода и кликаем по ней
         onView(withText("Выйти")).check(matches(isDisplayed())); // проверяем что всплывает кнопка Выйти
         onView(withText("Выйти")).perform(click()); // кликаем по всплывающей кнопке
         onView(withText("Авторизация")).check(matches(withText("Авторизация"))); // проверяем что отображается страница для входа
+        Thread.sleep(8500);
+//        SystemClock.sleep(3000);
     }
 
     @Test
-    public void logInWithInValidData() { // падает если авторизирован в приложении
+    public void logInWithInValidData() throws InterruptedException { // падает если авторизирован в приложении
         onView(allOf(withHint("Логин"))).perform(replaceText(invalidLogin)).check(matches(withText("5login"))); // вводим логин
         onView(allOf(withHint("Пароль"))).perform(replaceText(invalidPassword)).check(matches(withText("password"))); // вводим пароль
         closeSoftKeyboard(); // скрываем клавиатуру ввода
@@ -83,22 +80,45 @@ public class Authorization {
         onView(withText("Неверный логин или пароль"))
                 .inRoot(withDecorView(not(is(activityTestRule.getActivity().getWindow().getDecorView()))))
                 .check(matches(isDisplayed()));
+        Thread.sleep(7500);
+//        SystemClock.sleep(3000);
     }
 
+    @Test
+    public void logInWithInValidLoginAndValidPassword() throws InterruptedException { // падает если авторизирован в приложении
+        onView(allOf(withHint("Логин"))).perform(replaceText(invalidLogin)).check(matches(withText("5login"))); // вводим логин
+        onView(allOf(withHint("Пароль"))).perform(replaceText(validPassword)).check(matches(withText("password2"))); // вводим пароль
+        closeSoftKeyboard(); // скрываем клавиатуру ввода
+        onView(withId(R.id.enter_button)).perform(click()); // кликаем по кнопке входа
+        onView(withText("Неверный логин или пароль"))
+                .inRoot(withDecorView(not(is(activityTestRule.getActivity().getWindow().getDecorView()))))
+                .check(matches(isDisplayed()));
+        Thread.sleep(7500);
+//        SystemClock.sleep(3000);
 
-//        @Test
-//    public void logInWithInValidLoginAndValidPassword() throws InterruptedException { // падает если авторизирован в приложении
+    }
 
+    @Test
+    public void logInWithValidLoginAndInvalidPassword() throws InterruptedException { // падает если авторизирован в приложении
+        onView(allOf(withHint("Логин"))).perform(replaceText(validLogin)).check(matches(withText("login2"))); // вводим логин
+        onView(allOf(withHint("Пароль"))).perform(replaceText(invalidPassword)).check(matches(withText("password"))); // вводим пароль
+        closeSoftKeyboard(); // скрываем клавиатуру ввода
+        onView(withId(R.id.enter_button)).perform(click()); // кликаем по кнопке входа
+        onView(withText("Неверный логин или пароль"))
+                .inRoot(withDecorView(not(is(activityTestRule.getActivity().getWindow().getDecorView()))))
+                .check(matches(isDisplayed()));
+        Thread.sleep(7500);
+//        SystemClock.sleep(3000);
+    }
 
-//    }
-//
-//    @Test
-//    public void logInWithValidLoginAndInvalidPassword() throws InterruptedException { // падает если авторизирован в приложении
+    @Test
+    public void logInWithEmptyData() throws InterruptedException { // падает если авторизирован в приложении
+        onView(withId(R.id.enter_button)).perform(click()); // кликаем по кнопке входа
+        onView(withText("Логин и пароль не могут быть пустыми"))
+                .inRoot(withDecorView(not(is(activityTestRule.getActivity().getWindow().getDecorView()))))
+                .check(matches(isDisplayed()));
+        Thread.sleep(7500);
+//        SystemClock.sleep(3000);
+    }
 
-    //    }
-//
-//    @Test
-//    public void logInWithEmptyData() throws InterruptedException { // падает если авторизирован в приложении
-//
-//    }
 }
