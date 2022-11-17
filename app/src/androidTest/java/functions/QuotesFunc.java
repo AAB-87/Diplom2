39317d2b926da7e1f9c00d3fa5480676b40099bf
AAB-87 <andrej-abrekov@yandex.ru>
2022-11-17 21:57:55 +0400

    добавил тесты на страницу цитат

---
+++ app/src/androidTest/java/functions/QuotesFunc.java
@@ -0,0 +1,32 @@
+package functions;
+
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import data.DataHelper;
+import io.qameta.allure.kotlin.Allure;
+import screenElements.QuotesScreen;
+
+public class QuotesFunc {
+
+    public static void openOrCloseFirstQuote() {
+        Allure.step("Раскрыть/свернуть первую цитату");
+        QuotesScreen.openOrCloseFirstQuoteButton.perform(click());
+        // проверка шага не предусмотрена (результат зависит от исходногго состояния)
+    }
+
+    public static void checkThatFirstQuoteContentIsFull() {
+        Allure.step("Проверить содержимое первой цитаты");
+        QuotesScreen.firstMissionTitleValue.check(matches(isDisplayed()));
+        QuotesScreen.firstDescriptionOfQuoteValue.check(matches(isDisplayed()));
+    }
+
+    public static void findQuoteWith(String title) {
+        Allure.step("Поиск цитаты с заголовком");
+        DataHelper.isDisplayedWithSwipe(onView(withText(title)), 3, true);
+    }
+
+}
